3be8f3f7c411c021e5040509a702045e1fb2195c
zanhdada1024-sketch <zanhdada1024@gmail.com>
2026-05-11 11:43:16 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,10 @@
 日麻點數計算助手 - 源代碼模塊
 """
 
-from tile_parser import TileParser
-from hand_analyzer import HandAnalyzer
-from yaku_detector import YakuDetector
-from points_calculator import PointsCalculator
+from .tile_parser import TileParser
+from .hand_analyzer import HandAnalyzer
+from .yaku_detector import YakuDetector
+from .points_calculator import PointsCalculator
 
 __all__ = [
     'TileParser',
